872c9393acd8bb1af1d797bb72fac7746808fda7
NishanWijethunga <nishansameerawijethunga@gmail.com>
2020-03-29 07:15:01 +0530

    updated.

--- src/app/shared/material-components/material-components.module.ts
+++ src/app/shared/material-components/material-components.module.ts
@@ -5,10 +5,11 @@ import { MatRippleModule } from '@angular/material/core'
 import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatIconModule } from '@angular/material/icon'
 import { MatListModule } from '@angular/material/list'
+import { MatCardModule } from '@angular/material/card'
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatButtonModule, MatRippleModule, MatSidenavModule, MatIconModule, MatListModule],
-  exports: [MatButtonModule, MatRippleModule, MatSidenavModule, MatIconModule, MatListModule]
+  imports: [CommonModule, MatButtonModule, MatRippleModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule],
+  exports: [MatButtonModule, MatRippleModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule]
 })
 export class MaterialComponentsModule {}
--- src/app/view/home/home.module.ts
+++ src/app/view/home/home.module.ts
@@ -1,15 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
 
-import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home.component';
-
+import { HomeRoutingModule } from './home-routing.module'
+import { HomeComponent } from './home.component'
+import { MaterialComponentsModule } from 'src/app/shared/material-components/material-components.module'
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  imports: [CommonModule, HomeRoutingModule, MaterialComponentsModule]
 })
-export class HomeModule { }
+export class HomeModule {}
