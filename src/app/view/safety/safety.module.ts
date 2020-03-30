2ac00ad4781ea1ac286623cb296368410e270076
NishanWijethunga <nishansameerawijethunga@gmail.com>
2020-03-30 13:14:12 +0530

    updated.

--- src/app/view/safety/safety.module.ts
+++ src/app/view/safety/safety.module.ts
@@ -1,15 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
 
-import { SafetyRoutingModule } from './safety-routing.module';
-import { SafetyComponent } from './safety.component';
-
+import { SafetyRoutingModule } from './safety-routing.module'
+import { SafetyComponent } from './safety.component'
+import { MaterialComponentsModule } from 'src/app/shared/material-components/material-components.module'
+import { FeatherModule } from 'angular-feather'
+import { allIcons } from 'angular-feather/icons'
 
 @NgModule({
   declarations: [SafetyComponent],
-  imports: [
-    CommonModule,
-    SafetyRoutingModule
-  ]
+  imports: [CommonModule, SafetyRoutingModule, MaterialComponentsModule, FeatherModule.pick(allIcons)]
 })
-export class SafetyModule { }
+export class SafetyModule {}
--- src/app/view/view.module.ts
+++ src/app/view/view.module.ts
@@ -6,10 +6,11 @@ import { ViewComponent } from './view.component'
 import { MaterialComponentsModule } from '../shared/material-components/material-components.module'
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component'
 import { SidenavContentComponent } from './components/sidenav-content/sidenav-content.component'
-import { HomeModule } from './home/home.module'
+import { HomeModule } from './home/home.module';
+import { FooterComponent } from './components/footer/footer.component'
 
 @NgModule({
-  declarations: [ViewComponent, NavigationBarComponent, SidenavContentComponent],
+  declarations: [ViewComponent, NavigationBarComponent, SidenavContentComponent, FooterComponent],
   imports: [CommonModule, ViewRoutingModule, MaterialComponentsModule, HomeModule]
 })
 export class ViewModule {}
