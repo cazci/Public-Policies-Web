6316f9c6d26e25c3f680afbbfa76f3162509ddd9
NishanWijethunga <nishansameerawijethunga@gmail.com>
2020-03-31 01:02:55 +0530

    updated.

--- src/app/view/mobile/mobile.module.ts
+++ src/app/view/mobile/mobile.module.ts
@@ -1,15 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
 
-import { MobileRoutingModule } from './mobile-routing.module';
-import { MobileComponent } from './mobile.component';
-
+import { MobileRoutingModule } from './mobile-routing.module'
+import { MobileComponent } from './mobile.component'
+import { MaterialComponentsModule } from 'src/app/shared/material-components/material-components.module'
 
 @NgModule({
   declarations: [MobileComponent],
-  imports: [
-    CommonModule,
-    MobileRoutingModule
-  ]
+  imports: [CommonModule, MobileRoutingModule, MaterialComponentsModule]
 })
-export class MobileModule { }
+export class MobileModule {}
