dd2a3b376723654b6cc06e8a0e4c5b492e45316d
NishanWijethunga <nishansameerawijethunga@gmail.com>
2020-03-30 16:57:48 +0530

    env added.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  API_URL: 'http://167.71.48.57:8888/api'
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
